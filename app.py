85ec748999cb2a31a40fa66273a985a7f877698d
Nahomi <nahomijrojase@gmail.com>
2024-12-02 01:22:25 -0400

    Version inicial del proyecto

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+conn = get_db_connection()
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS reportes (
+        id SERIAL PRIMARY KEY,
+        paciente TEXT,
+        fecha TEXT,
+        sintomas TEXT,
+        observaciones TEXT,
+        diagnostico TEXT,
+        medico TEXT
+    )
+''')
+conn.commit()
+conn.close()
+
